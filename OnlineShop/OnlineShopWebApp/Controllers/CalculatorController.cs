dece1c8038436f57629c847e520dac28cacfc515
IvanovaAM <okshurochka@yandex.ru>
2025-06-06 19:31:02 +0300

    Создан контроллер Calculator с методом Index. Изменен шаблон запроса в классе Program. Изменен номер порта.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+	public class CalculatorController : Controller
+	{
+		public string Index(double num1, double num2, char sign)
+		{
+			switch(sign)
+			{
+				case '+':
+					return $"{num1} {sign} {num2} = {num1 + num2}";
+				case '-':
+					return $"{num1} {sign} {num2} = {num1 - num2}";
+				case '*':
+					return $"{num1} {sign} {num2} = {num1 * num2}";
+				default:
+					return $"Недопустимая операция!";
+			}
+			
+		}
+	}
+}
