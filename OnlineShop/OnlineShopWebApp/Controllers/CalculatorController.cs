b2d9c7a3ead2e2cf85a3b73dbd3581247f53c939
IvanovaAM <okshurochka@yandex.ru>
2025-06-06 19:57:51 +0300

    Реализован вывод пользователю информации о корректном вводе символа операции.

--- OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -4,20 +4,26 @@ namespace OnlineShopWebApp.Controllers
 {
 	public class CalculatorController : Controller
 	{
-		public string Index(double num1, double num2, char sign)
+		private readonly string[] signs = ["+", "-", "*"]; 
+
+		public string Index(double num1, double num2, string sign)
 		{
-			switch(sign)
+			if (sign.Length >= 2 || !signs.Contains(sign))
 			{
-				case '+':
-					return $"{num1} {sign} {num2} = {num1 + num2}";
-				case '-':
-					return $"{num1} {sign} {num2} = {num1 - num2}";
-				case '*':
-					return $"{num1} {sign} {num2} = {num1 * num2}";
-				default:
-					return $"Недопустимая операция!";
+				return  $"Недопустимая операция!\nВ последнем сегменте может быть указан только один знак арифметической операции.\nДопустимые варианты символов: +, - , *";
 			}
-			
+
+			switch (sign)
+			{
+				case "-":
+					return $"{num1} {sign} {num2} = {num1 - num2}";
+
+				case "'*'":
+					return $"{num1} {sign} {num2} = {num1 * num2}";
+
+				default:
+					return $"{num1} {sign} {num2} = {num1 + num2}";
+			}
 		}
 	}
 }
