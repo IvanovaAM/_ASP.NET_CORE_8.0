b0c7f1f1b55c9f1349bcfc852ce865b37a340d26
IvanovaAM <okshurochka@yandex.ru>
2025-06-07 00:42:54 +0300

    Создан контроллер Calc с методом Index. Изменен номер порта. Реализован вывод пользователю информации о корректном вводе символа операции.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+	public class CalcController : Controller
+	{
+		public string Index(double a=0, double b=0, string c="+")
+		{
+			if (c.Length >= 2 || !c.Contains(c))
+			{
+				return $"Недопустимая операция!\nВ последнем сегменте может быть указан только один знак арифметической операции.\nДопустимые варианты символов: +, - , *";
+			}
+
+			switch (c)
+			{
+				case "-":
+					return $"{a} {c} {b} = {a - b}";
+
+				case "*":
+					return $"{a} {c} {b} = {a * b}";
+
+				case "/":
+					return (b != 0) ? $"{a} {c} {b} = {a / b}" : $"На ноль делить нельзя! Введите другое значение параметра b";
+
+				default:
+					return $"{a} {c} {b} = {a + b}";
+			}
+		}
+	}
+}
