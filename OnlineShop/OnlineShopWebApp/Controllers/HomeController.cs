f285c4a856905935cf71d8286885f81feac2a308
IvanovaAM <okshurochka@yandex.ru>
2025-06-08 01:47:19 +0300

    Согласно разбору ДЗ  в классе Product добавлено закрытое статическое поле, инициализирующее Id. Убраны setters, так как инциализация полей  будет в конструкторе. В контроллере Home инициализация листа перенесена в статическую переменную для того, что при обновлении страниц и повторном создании объекта контроллера лист не инициализировался заново.

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,33 +1,35 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShopWebApp.Models;
+using System.Diagnostics;
 
 namespace OnlineShopWebApp.Controllers
 {
-    public class HomeController : Controller
-    {
-		private readonly List<Product> products = [];
+	public class HomeController : Controller
+	{
+		private static readonly List<Product> products =
+			[
+				new Product("Корм для собак мелких пород, 5 кг", 2590.2M, "Корм для собак мелких пород, 5 кг"),
+				new Product("Корм для собак средних пород, 5 кг", 3080.8M, "Корм для собак средних пород, 5 кг"),
+				new Product("Корм для собак крупных пород, 5 кг", 3770.3M, "Корм для собак крупных пород, 5 кг")
+			];
 
 		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-			products.Add(new Product(1, "Корм для собак мелких пород, 5 кг", 2590.2M, "Корм для собак мелких пород, 5 кг"));
-			products.Add(new Product(2, "Корм для собак средних пород, 5 кг", 3080.8M, "Корм для собак средних пород, 5 кг"));
-			products.Add(new Product(3, "Корм для собак крупных пород, 5 кг", 3770.3M, "Корм для собак крупных пород, 5 кг"));
+		public HomeController(ILogger<HomeController> logger)
+		{
 
 			_logger = logger;
-        }
+		}
 
-        public IActionResult Index()
-        {
-            return View(products);
-        }
+		public IActionResult Index()
+		{
+			return View(products);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
