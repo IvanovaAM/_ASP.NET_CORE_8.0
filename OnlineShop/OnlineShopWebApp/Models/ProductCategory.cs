f975523c8f33ef3cf4f743f85f5b0e8e2597cbd1
IvanovaAM <okshurochka@yandex.ru>
2025-06-14 02:08:44 +0300

    Создано представление для каталога. Создано представление для продукта. Добавлены кнопки перехода на основную страницу идобавления в корзину. Созданы классы ProductCategory и ProductCategoryRepository. Добавлена единая для всех картинка товара.

---
+++ OnlineShop/OnlineShopWebApp/Models/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+
+namespace OnlineShopWebApp.Models
+{
+	public class ProductCategory(string name)
+	{
+		private static int instanceCounter = 1;
+		public int Id { get; } = instanceCounter++;
+		public string Name { get; } = name;
+	}
+}
